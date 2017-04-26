830f7768162f60a8b7786637fa697fe0a6eb67e6
Scott Robbins <scott.robbins@mobiledefense.com>
2017-04-26 00:41:38 -0400

    Add empty UITableView to MenuViewController in example project

--- Example/DeclarativeLayout/MenuViewController.swift
+++ Example/DeclarativeLayout/MenuViewController.swift
@@ -1,11 +1,54 @@
 import UIKit
+import DeclarativeLayout
 
 class MenuViewController: UIViewController {
+    
+    private let tableView = UITableView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        layout()
+        configureTableView()
+    }
+    
+    private func layout() {
+        
+        view.updateLayoutTo { (layout) in
+            
+            layout.add(tableView) { (layout) in
+                
+                layout.activate([
+                    layout.view.leadingAnchor.constraint(equalTo: layout.superview.leadingAnchor),
+                    layout.view.trailingAnchor.constraint(equalTo: layout.superview.trailingAnchor),
+                    layout.view.topAnchor.constraint(equalTo: layout.superview.topAnchor),
+                    layout.view.bottomAnchor.constraint(equalTo: layout.superview.bottomAnchor),
+                ])
+            }
+        }
+    }
+    
+    private func configureTableView() {
+        tableView.delegate = self
+        tableView.dataSource = self
     }
 
 }
 
+extension MenuViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 0
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return UITableViewCell()
+    }
+}
+
+extension MenuViewController: UITableViewDelegate {
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+    }
+}
