853a24d69f454c5078ebbf19220102d15312f7ec
Scott Robbins <scott.robbins@mobiledefense.com>
2017-12-10 22:23:21 -0500

    Make LayoutConstraint Hashable

--- DeclarativeLayout/Classes/LayoutConstraint.swift
+++ DeclarativeLayout/Classes/LayoutConstraint.swift
@@ -52,3 +52,39 @@ extension LayoutConstraint: Equatable {
             && lhsConstraint.multiplier == rhsConstraint.multiplier
     }
 }
+
+extension LayoutConstraint: Hashable {
+    
+    var hashValue: Int {
+        
+        return combineHashes([
+            wrappedConstraint.firstItem?.hashValue ?? 0,
+            wrappedConstraint.secondItem?.hashValue ?? 0,
+            wrappedConstraint.firstAttribute.hashValue,
+            wrappedConstraint.secondAttribute.hashValue,
+            wrappedConstraint.relation.hashValue,
+            wrappedConstraint.multiplier.hashValue,
+            0,
+        ])
+    }
+    
+    /*
+                Credit to Sourcery's algorithm found here: https://github.com/krzysztofzablocki/Sourcery/blob/3e69bec7df09fb8ea42a927c4a8a2c82a196075b/Templates/Tests/Expected/AutoHashable.expected
+                This is the boost hash combine algorithm implemented in Swift
+        */
+    fileprivate func combineHashes(_ hashes: [Int]) -> Int {
+        return hashes.reduce(0, combineHashValues)
+    }
+    
+    fileprivate func combineHashValues(_ initial: Int, _ other: Int) -> Int {
+        #if arch(x86_64) || arch(arm64)
+            let magic: UInt = 0x9e3779b97f4a7c15
+        #elseif arch(i386) || arch(arm)
+            let magic: UInt = 0x9e3779b9
+        #endif
+        var lhs = UInt(bitPattern: initial)
+        let rhs = UInt(bitPattern: other)
+        lhs ^= rhs &+ magic &+ (lhs << 6) &+ (lhs >> 2)
+        return Int(bitPattern: lhs)
+    }
+}
