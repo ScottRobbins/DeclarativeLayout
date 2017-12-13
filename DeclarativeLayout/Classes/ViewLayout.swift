46e46db5e7a57ebc6f92022fa09c0c9c1b4b5735
Scott Robbins <scott.robbins@mobiledefense.com>
2017-12-13 02:42:51 -0500

    Speed up removal of unneeded subviews
    By using a HashSet, as it goes through the current layout's subviews,
    when it asks if the new layout's subviews contains a view, that
    contains operation will be a O(1) operation instead of O(n).
    I have profiled this to make that process ~40% faster when running
    the layout update a second time.

--- DeclarativeLayout/Classes/ViewLayout.swift
+++ DeclarativeLayout/Classes/ViewLayout.swift
@@ -51,8 +51,9 @@ public class ViewLayout<T: UIView> {
     }
     
     private func removeUnneededSubviews(with layoutComponent: UIViewLayoutComponent<T>) {
+        let layoutComponentSubviews = HashSet(layoutComponent.allSubviews())
         currentLayoutComponent.allSubviews()
-            .filter { !layoutComponent.allSubviews().contains($0) }
+            .filter { !layoutComponentSubviews.contains($0) }
             .forEach { $0.removeFromSuperview() }
     }
     
