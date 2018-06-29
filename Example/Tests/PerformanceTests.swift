b77c9ef3963c2634401b495736302b8f6ec513e0
Scott Robbins <scott.robbins@mobiledefense.com>
2018-06-29 01:46:56 -0400

    Add initial test

--- Example/Tests/PerformanceTests.swift
+++ Example/Tests/PerformanceTests.swift
@@ -4,25 +4,20 @@ import DeclarativeLayout
 
 class PerformanceTests: XCTestCase {
     
+    var view: ExampleView!
+    var viewLayout: ViewLayout<ExampleView>!
+    
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        view = ExampleView()
+        viewLayout = ViewLayout(view: view)
     }
     
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // This is an example of a functional test case.
-        XCTAssert(true, "Pass")
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
+    func testInitialLayout() {
+        
         self.measure() {
-            // Put the code you want to measure the time of here.
+            view.layoutAndConfigure(with: viewLayout)
         }
     }
     
