d08fbce905925be925f31cf65deaa036d02c065c
Scott Robbins <scott.robbins@mobiledefense.com>
2019-02-24 19:50:43 -0500

    Add SnapKit pod

--- Example/Pods/Target Support Files/Pods-DeclarativeLayout_Example/Pods-DeclarativeLayout_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-DeclarativeLayout_Example/Pods-DeclarativeLayout_Example-frameworks.sh
@@ -145,10 +145,12 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Anchorage/Anchorage.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/DeclarativeLayout/DeclarativeLayout.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SnapKit/SnapKit.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/Anchorage/Anchorage.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/DeclarativeLayout/DeclarativeLayout.framework"
+  install_framework "${BUILT_PRODUCTS_DIR}/SnapKit/SnapKit.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
   wait
