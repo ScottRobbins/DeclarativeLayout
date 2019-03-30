80f5c243ef1eee728cc7e059050166f30ad26def
Scott Robbins <scott.robbins@mobiledefense.com>
2019-03-30 15:42:27 -0400

    Update for Xcode 10.2

--- CachedLayoutObjectStore.swift
+++
@@ -1,7 +0,0 @@
-import UIKit
-
-final class CachedLayoutObjectStore {
-    var viewStorage = [String: UIView]()
-    var stackViewStorage = [String: UIStackView]()
-    var layoutGuideStorage = [String: UILayoutGuide]()
-}
